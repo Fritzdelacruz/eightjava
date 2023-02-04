7fe3762996ff9d0f91e661c6a2a81cb804f4ce46
Fritzdelacruz <119088250+Fritzdelacruz@users.noreply.github.com>
2023-02-04 16:43:22 +0800

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,34 @@
+let cart = {};
+if (localStorage.getItem("cart")) {
+    cart = JSON.parse(localStorage.getItem("cart"));
+}
+
+let tbody = document.getElementById("tbody");
+
+for (let id in cart) {
+    let item = cart[id];
+
+    let tr = document.createElement('tr')
+
+    var img = document.createElement('img');
+    img.src = 'https://www.thesprucepets.com/thmb/Wh_mSm5UjL7OOvqsyQgXgFatshc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-181861505-4e63227ed0a14dc9bfe86848ef54caf3.jpg';
+    img.style.height = '150px'
+    img.style.width = '140px'
+    tr.appendChild(img);
+
+    let title_td = document.createElement('td');
+    title_td.textContent = item.title;
+    tr.appendChild(title_td);
+
+
+    let price_td = document.createElement("td");
+    price_td.textContent = item.price;
+    tr.appendChild(price_td);
+
+    let qty_td = document.createElement("td");
+    qty_td.textContent = item.qty;
+    tr.appendChild(qty_td);
+
+    tbody.appendChild(tr)
+
+}
